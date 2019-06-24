1aa15df217fe43ef72eae244b55d8db30b9af75b
limwonki0619 <limwk0619@naver.com>
2019-06-24 14:02:12 +0900

    가설검정

---
+++ Ch_06_가설검정/sec02_단일 모집단의 가설검정.R
@@ -0,0 +1,16 @@
+setwd("D:/limworkspace/R_Statistics/Ch_06_가설검정")
+
+library(extrafont)
+
+windowsFonts(hanna=windowsFont("BM HANNA 11yrs old")) # 폰트이름 변경 
+windowsFonts(dohyeon=windowsFont("BM DoHyeon"))
+windowsFonts(jalnan=windowsFont("Jalnan"))
+windowsFonts(tb=windowsFont("TmonMonsori Black"))
+windowsFonts(binggrae=windowsFont("Binggrae Taom"))
+windowsFonts(nbg=windowsFont("나눔바른고딕"))
+
+library(dplyr)
+library(ggplot2)
+library(reshape)
+
+# Section 2. 단일 모집단의 가설검정 -----------------------------------------------------------------------------------------------------------------------------------------
