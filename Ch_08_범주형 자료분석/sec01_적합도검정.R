1f5f3cfa89b4c2a6356a768889d68f05d8a2cbbb
limwonki0619 <limwk0619@naver.com>
2019-06-27 12:13:31 +0900

    범주형 자료분석

---
+++ Ch_08_범주형 자료분석/sec01_적합도검정.R
@@ -0,0 +1,42 @@
+setwd("D:/limworkspace/R_Statistics/Ch_08_범주형 자료분석")
+# 참고자료 : https://rfriend.tistory.com/137
+
+
+# 범주형 자료분석 : 값으로부터 직접 계산을 할 수 없는 질력자료의 경우 실시할 수 있는 분석이다. 여기서 중요한 계산은
+# 질적자료의 각 범주의 개수를 세는 것이다.
+
+# Section 2. 동질섬 검정
+
+# 가설수립
+# H1 : p1=9/16, p2=3/16, p3=3/16, p4=1/16
+# H0 : not H1
+
+# 기대도수 (Expected Frequency, E)
+# 전체개수 * 각 범주의 영가설 하에서의 비율 
+# E1 = 556 * 9/16, E2 = 556*6/16, E3 = 556*3/16, E4 = 556*1/16
+
+# 관찰도수 
+# 실제 관찰된 도수(Observed frequency, O)
+
+# chisq(k-1) 검정통계량 
+# 모집단을 k로 나누는 범주형 자료에 대해, i번쨰 범주의 관찰도수를 Oi, 기대도수를 Ei라고 하면,
+# 카이제곱 검정통계량은 자유도가 (범주[k]-1)인 카이제곱 분포를 따른다. 
+
+# R함수 chisq.test()를 이용한 검정 
+x <- c(315, 101, 108, 32)
+chisq.test(x, p=c(9,3,3,1)/16)
+
+axis_x <- seq(0,15,by=0.01)
+data <- data.frame(x=axis_x,
+                   y=dchisq(axis_x, df=3))
+1-pchisq(2.14, df=3)
+1-pchisq(7.81, df=3)
+1-pchisq(0.470024, df=3)
+qchisq(1-0.05, df=3)
+qchisq(1-0.9254, df=3)
+library(ggplot2)
+ggplot(data, aes(x=x,y=y)) + 
+  geom_line() +
+  geom_area(data=data[data$x > qchisq(1-0.9254, df=3),], alpha=0.2) +
+  geom_area(data=data[data$x > qchisq(1-0.05, df=3),], fill="red", alpha=0.5)
+?geom.pol
---
+++ Ch_08_범주형 자료분석/sec02_동질성검정과 독립성검정.R
@@ -0,0 +1,45 @@
+setwd("D:/limworkspace/R_Statistics/Ch_08_범주형 자료분석")
+# 참고자료 : https://rfriend.tistory.com/137
+
+# Section 2. 동질성 검정과 독립성 검정 
+
+# 1. 동질성 검정 
+
+sns.c <- read.csv("data/snsbyage.csv",header = T, stringsAsFactors = F)
+sns.c <- transform(sns.c, service.c = 
+                     factor(service, levels = c("F","T","K","C","E"), ordered = T))
+
+sns.c <- transform(sns.c, age.c = 
+                     factor(age, levels = c("1","2","3"), labels = c("20대","30대","40대"),ordered=T))
+
+str(sns.c)
+
+c.tab <- table(sns.c$age.c, sns.c$service.c)
+a.n <- margin.table(c.tab, margin = 1) # 행 기준 더하기 
+s.n <- margin.table(c.tab, margin = 2) # 열 기준 더하기 
+s.p <- s.n / margin.table(c.tab)
+expected <- a.n %*% t(s.p)
+
+o.e <- c.tab-expected
+t.t <- sum( (o.e)^2 / expected)
+1-pchisq(t.t, df=8)
+
+chisq.test(c.tab)
+
+# 2. 독립성 검정
+# 어떤 모집단에서 관찰한 두 개의 속성 R과 C가 범주형 변수일 때 두 변수가 서로 연관이 있는지를 검정하는 것 
+
+# 예제 3. 성별에 따른 대학원 입학 여부의 독립성 검정 
+data(UCBAdmissions)
+str(UCBAdmissions)
+
+ucba.tab <- apply(UCBAdmissions, c(1,2), sum)
+chisq.test(ucba.tab, correct = F) # 예이츠 보정의 목적은 통계적 유의성을 과대 평가하는 것을 막기 위한 것으로 (특히 표본수가 작은 경우) 기본이 보정을 하는 것으로 되어 있습니다. 만약 하지 않으려면 correct=F의 옵션을 주면 됩니다. 
+
+
+
+data1 <- data.frame(x=seq(-3,3,0.01),
+                    norm=dnorm(seq(-3,3,0.01)),
+                    t=dt(seq(-3,3,0.01),3))
+
+
